271f27297cb8ed29abf5e2f1ca60d7fb071ee90d
loic-msgb <loic.missigbeto@isen.yncrea.fr>
2023-06-25 19:19:43 +0200

    changement led, contrôle gestion de partie

--- src/Arduino/arduino/affichage.py
+++ src/Arduino/arduino/affichage.py
@@ -1,74 +1,127 @@
-import tkinter as tk
 import serial
-import threading
-import time
+import tkinter as tk
 
-# Configurer la communication série
-ser = serial.Serial('COM3', 9600)
+# Définir les paramètres de communication série
+port = 'COM3'  # Remplacez par le port série approprié
+baudrate = 9600  # Assurez-vous que cela correspond à la vitesse série définie dans votre code Arduino
 
-# Créer une fenêtre Tkinter
+# Créer la fenêtre principale de l'interface graphique
 window = tk.Tk()
-window.title("Affichage du tableau")
+window.title("MorpionGéant")
 
-# Créer une étiquette pour le texte de bienvenue centré en haut de la fenêtre
-welcome_label = tk.Label(window, text="Bienvenue dans le Morpion Géant", font=("Arial", 16))
-welcome_label.pack(pady=20)
+# Ajouter une étiquette pour le texte de bienvenue
+welcome_label = tk.Label(window, text="Bienvenue sur notre MorpionGéant ! Amusez-vous bien", font=('Arial', 14))
+welcome_label.pack(pady=10)
 
-# Créer un cadre pour le plateau de morpion
-board_frame = tk.Frame(window)
-board_frame.pack()
+# Créer une grille pour représenter le plateau de morpion
+board = [[None, None, None],
+         [None, None, None],
+         [None, None, None]]
 
-# Créer une grille de boutons pour représenter le tableau
-buttons = []
+# Créer des boutons pour représenter les cases du morpion
+buttons = [[tk.Button(window, width=10, height=5, font=('Arial', 20, 'bold'), bg='white') for _ in range(3)] for _ in range(3)]
+
+# Placer les boutons dans la grille
 for i in range(3):
-    row = []
     for j in range(3):
-        button = tk.Button(board_frame, text=" ", width=10, height=5)
-        button.grid(row=i, column=j, padx=5, pady=5)
-        row.append(button)
-    buttons.append(row)
+        buttons[i][j].grid(row=i+1, column=j, padx=5, pady=5)
 
-def update_gui():
-    if ser.in_waiting > 0:
-        # Lire les données série
-        data = ser.readline().decode().rstrip()
-        
-        # Extraire les valeurs individuelles du message
-        values = data.split(';')
-        
-        for value in values:
-            # Vérifier si la ligne de données contient les quatre éléments attendus
-            if ',' in value:
-                # Extraire les indices, l'état et l'équipe de chaque case
-                i, j, etat, equipe = value.split(',')
-                try:
-                    i = int(i)
-                    j = int(j)
-                    etat = int(etat)
-                    equipe = int(equipe)
-                    
-                    # Vérifier si les indices sont dans la plage valide (0-2)
-                    if 0 <= i < 3 and 0 <= j < 3:
-                        # Mettre à jour le texte du bouton correspondant avec le numéro de l'équipe
-                        buttons[i][j].configure(text=str(equipe))
-                except ValueError:
-                    # Ignorer les lignes de données incorrectes
-                    pass
+# Ouvrir la connexion série
+ser = serial.Serial(port, baudrate)
 
-    # Rafraîchir la fenêtre Tkinter
-    window.update()
+# Attendre une seconde pour laisser le temps à la connexion de s'établir
+ser.timeout = 1
 
-def update_gui_periodic():
+# Variable pour contrôler l'exécution du jeu
+jeuEnCours = False
+
+# Fonction pour mettre à jour l'état des cases
+def update_case_state(data):
+    # Nettoyer le plateau de morpion
+    for i in range(3):
+        for j in range(3):
+            board[i][j] = None
+            buttons[i][j].config(text="", state=tk.NORMAL, bg='white')
+
+    # Analyser les données
+    case_data = data.split(";")
+    for case_info in case_data:
+        case_info_parts = case_info.split(",")
+        if len(case_info_parts) == 4:
+            i = int(case_info_parts[0])
+            j = int(case_info_parts[1])
+            etat = int(case_info_parts[2])
+            equipe = int(case_info_parts[3])
+
+            # Mettre à jour l'état de la case sur le plateau de morpion
+            board[i][j] = equipe
+            buttons[i][j].config(text=str(equipe), state=tk.DISABLED)
+            if equipe == 1:
+                buttons[i][j].config(bg='red')
+            elif equipe == 2:
+                buttons[i][j].config(bg='blue')
+
+            # Vérifier la victoire
+            if check_victory(equipe):
+                stop_game()  # Arrêter le jeu si une équipe a gagné
+
+# Fonction pour lire les données série et mettre à jour l'interface
+def read_serial():
+    global jeuEnCours
     while True:
-        update_gui()
-        time.sleep(0.3)
+        if jeuEnCours:
+            data = ser.readline().decode().strip()  # Lire une ligne de données série et décoder en tant que chaîne de caractères
+            if data:
+                update_case_state(data)
+        window.update()
 
-# Lancer la mise à jour périodique de l'interface graphique dans un thread séparé
-update_thread = threading.Thread(target=update_gui_periodic)
-update_thread.start()
+# Fonction pour vérifier la victoire
+def check_victory(equipe):
+    # Vérifier les lignes
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] == equipe:
+            return True
 
-# Centrer la fenêtre au milieu de l'écran
-window.eval('tk::PlaceWindow . center')
+    # Vérifier les colonnes
+    for j in range(3):
+        if board[0][j] == board[1][j] == board[2][j] == equipe:
+            return True
 
-# Démarrer la boucle principale Tkinter
+    # Vérifier les diagonales
+    if board[0][0] == board[1][1] == board[2][2] == equipe:
+        return True
+    if board[0][2] == board[1][1] == board[2][0] == equipe:
+        return True
+
+    return False
+
+# Fonction pour arrêter le jeu
+def stop_game():
+    global jeuEnCours
+    jeuEnCours = False
+    ser.write(b'STOP\n')  # Envoyer l'instruction STOP via la communication série
+
+# Fonction pour démarrer le jeu
+def start_game():
+    global jeuEnCours
+    jeuEnCours = True
+    ser.write(b'START\n')  # Envoyer l'instruction START via la communication série
+
+# Créer les boutons de contrôle du jeu
+start_button = tk.Button(window, text="Démarrer le jeu", font=('Arial', 12), command=start_game)
+start_button.pack(pady=10)
+
+stop_button = tk.Button(window, text="Arrêter le jeu", font=('Arial', 12), command=stop_game)
+stop_button.pack(pady=5)
+
+# Démarrer la lecture des données série dans un thread séparé
+import threading
+thread = threading.Thread(target=read_serial)
+thread.daemon = True
+thread.start()
+
+# Démarrer la boucle principale de l'interface graphique
 window.mainloop()
+
+# Fermer la connexion série
+ser.close()
--- src/Arduino/arduino/arduino.ino
+++ src/Arduino/arduino/arduino.ino
@@ -14,12 +14,15 @@ const int valeurLumiereCartonMin = 700;
 Case cases[3][3] = {
   {Case(A0, 13), Case(A1, 12), Case(A2, 11)},
   {Case(A3, 10), Case(A4, 9), Case(A5, 8)},
-  {Case(A5, 7), Case(A5, 6), Case(A5, 5)}
+  {Case(A3, 7), Case(A3, 6), Case(A3, 5)}
 };
+
+bool jeuEnCours = false; // Variable pour contrôler l'exécution du jeu
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(11, OUTPUT);
   for (size_t i = 0; i < 3; i++) {
     for (size_t j = 0; j < 3; j++) {
       pinMode(cases[i][j].pinLed, OUTPUT);
@@ -28,38 +31,59 @@ void setup() {
 }
 
 void loop() {
-  // Mettez votre code principal ici, à exécuter en boucle :
+  if (jeuEnCours) {
+    // Code du jeu à exécuter uniquement si le jeu est en cours
 
-  String data; // Chaîne de caractères pour stocker les données sérialisées
+    String data; // Chaîne de caractères pour stocker les données sérialisées
 
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      int valeurAnalogique = analogRead(cases[i][j].pinCapteurLumiere);
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        int valeurAnalogique = analogRead(cases[i][j].pinCapteurLumiere);
 
-      if (valeurAnalogique <= valeurLumiereJour) {
-        cases[i][j].liberer();
-        digitalWrite(cases[i][j].pinLed, LOW);
+        if (valeurAnalogique <= valeurLumiereJour) {
+          cases[i][j].liberer();
+          digitalWrite(cases[i][j].pinLed, HIGH);
+        }
+        else if (valeurAnalogique <= valeurLumierePlexiMax) {
+          cases[i][j].occuper(2);
+          digitalWrite(cases[i][j].pinLed, LOW);
+        }
+        else if (valeurAnalogique >= valeurLumiereCartonMin) {
+          cases[i][j].occuper(1);
+          digitalWrite(cases[i][j].pinLed, LOW);
+        }
+
+        // Ajouter les informations de la case à la chaîne de données
+        data += String(i) + "," + String(j) + "," + String(cases[i][j].etat) + "," + String(cases[i][j].equipe) + ";";
       }
-      else if (valeurAnalogique <= valeurLumierePlexiMax) {
-        cases[i][j].occuper(2);
-        digitalWrite(cases[i][j].pinLed, HIGH);
-      }
-      else if (valeurAnalogique >= valeurLumiereCartonMin) {
-        cases[i][j].occuper(1);
-        digitalWrite(cases[i][j].pinLed, HIGH);
-      }
-
-      // Ajouter les informations de la case à la chaîne de données
-      data += String(i) + "," + String(j) + "," + String(cases[i][j].etat) + "," + String(cases[i][j].equipe) + ";";
-
+      delay(100);
     }
-    delay(100);
+
+    // Envoyer les données sérialisées via la communication série
+    Serial.println(data);
+    // Effacer la chaîne de données pour la prochaine itération
+    data = "";
+  }
+  else {
+    // Attendre une instruction START pour démarrer le jeu
+    while (Serial.available() > 0) {
+      String instruction = Serial.readStringUntil('\n');
+      if (instruction == "START") {
+        jeuEnCours = true;
+        break;
+      }
+    }
   }
 
-  // Envoyer les données sérialisées via la communication série
-  Serial.println(data);
-  // Effacer la chaîne de données pour la prochaine itération
-  data = "";
-  // Attendre 100ms avant de recommencer
+  // Vérifier si l'instruction STOP a été reçue pour arrêter le jeu
+  while (Serial.available() > 0) {
+    String instruction = Serial.readStringUntil('\n');
+    if (instruction == "STOP") {
+      jeuEnCours = false;
+      break;
+    }
+  }
+
+  // Attendre 100ms avant de recommencer la boucle
   delay(100);
 }
